a01b88d9773906ff7fd587930cec9eb9b204e4da
nogtk <taka.naoga@moneyforward.co.jp>
2021-01-23 21:01:45 +0900

    Add .gitconfig

--- make_symlink.sh
+++ make_symlink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .vimrc .gemrc .ideavimrc .pryrc .tigrc)
+dotfiles=(.zshrc .vimrc .gemrc .ideavimrc .pryrc .tigrc .gitconfig)
 
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
