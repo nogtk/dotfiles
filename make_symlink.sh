f6e38930334ca693d2afd58d887869030eabd6b0
nogtk <taka.naoga@moneyforward.co.jp>
2021-01-23 21:20:49 +0900

    Add Brewfile

--- make_symlink.sh
+++ make_symlink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(.zshrc .vimrc .gemrc .ideavimrc .pryrc .tigrc .gitconfig)
+files=(.zshrc .vimrc .gemrc .ideavimrc .pryrc .tigrc .gitconfig Brewfile)
 
 # ホームディレクトリ直下に作成する
-for file in "${dotfiles[@]}"; do
+for file in "${files[@]}"; do
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
